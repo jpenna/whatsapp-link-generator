2b2f4d6f1e2103fc5e8bcae52c5d92b75929166a
Juliano Penna <jpenna@churchcommunitybuilder.com>
2020-12-18 11:58:39 -0300

    Fix service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,15 @@
-const cacheName = 'zap';
+const cacheName = 'zap2';
 
-// Offline files
 const cacheFiles = [
   'offline.html',
   'style.css',
   'images/favicon.png',
-]
+  'index.html',
+];
 
 self.addEventListener('install', (event) => {
   console.log('[Service Worker] Install');
+  // self.skipWaiting();
   event.waitUntil(
     caches.open(cacheName).then((cache) => {
       return cache.addAll(cacheFiles);
@@ -16,23 +17,33 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  // console.log('[Service Worker] Forced Claim');
+  // clients.claim();
+
+  console.log('[Service Worker] Cleaning Cache');
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== cacheName) {
+          return caches.delete(key)
+        }
+      })
+    ))
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (!response && !navigator.onLine) {
-        return caches.match('offline.html');
-      }
-
-      if (response && navigator.onLine) {
-        return caches.open(cacheName).then((cache) =>
-          fetch(event.request).then((updated) => {
-            cache.put(event.request, updated.clone())
-            return updated
+    caches.match(event.request)
+    .then((found) =>
+      found ||
+      caches.open(cacheName).then((cache) =>
+        fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone())
+          return response
           })
         )
-      }
-
-      return fetch(event.request)
-    }).catch(console.err)
+    ).catch(console.err)
   );
 });
